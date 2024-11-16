a1227afd0b0085844541553946cb6b21edde1235
arevalocarlos2003 <00010423@uca.edu.sv>
2024-11-15 18:00:09 -0600

    feat: CLI Show Menu functions were created

--- src/main.cc
+++ src/main.cc
@@ -1,6 +1,7 @@
 #include <algorithm>
 #include <iostream>
 
+#include "cli_menu.h"
 #include "tree.h"
 
 int main(int argc, char const *argv[]) {
@@ -13,6 +14,41 @@ int main(int argc, char const *argv[]) {
   root->insert(currentMember);
 
   do {
+    ShowCLIMenuOptions();
+    std::cout << "\x1b[34mOption: \x1b[0m";
+    std::cin >> option;
+    switch (option) {
+      case 1:
+        /* code */
+        break;
+
+      case 2:
+        /* code */
+        break;
+
+      case 3:
+        do {
+          ShowCLISearchOptions();
+          std::cin >> option;
+          switch (option) {
+            case 1:
+              /* code */
+              break;
+
+            case 2:
+              /* code */
+              break;
+
+            case 3:
+              /* code */
+              break;
+          }
+        } while (option != 4);
+        break;
+
+      default:
+        break;
+    }
   } while (option != 8);
 
   delete root;
